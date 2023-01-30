d8b29a515ae2639e966cc4f1a5baab119df95705
sharmaharsh897 <sharmaharsh897@gmail.com>
2023-01-30 09:17:35 +0530

    added changes

--- app/serializers/post.js
+++ app/serializers/post.js
@@ -3,7 +3,8 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend ({
     normalizeResponse(store,primaryModelClass,payload,id,requestType){
         console.log(payload);
-        return this._super(...arguments);
+        payload= {posts: payload};  
+        return this._super(store,primaryModelClass,payload,id,requestType);
     }
 
 });
